e8e3dc2d9cde9aa0429ed4cdd8fcd8f8dcab9eb3
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-10-03 21:11:11 +0300

    Create branch feature/hibernate

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,4 @@
-create schema info;
-
-use info;
-CREATE TABLE CUSTOMERS(
+CREATE TABLE customer(
     id int auto_increment,
     name varchar(255),
     surname varchar(255),
@@ -9,20 +6,20 @@ CREATE TABLE CUSTOMERS(
     phone_number varchar(255),
     PRIMARY KEY (id));
 
-INSERT INTO CUSTOMERS(name, surname, age, phone_number) VALUES('Иван', 'Иванов', 21, '+799999999999');
-INSERT INTO CUSTOMERS(name, surname, age, phone_number) VALUES('алексей', 'Иванов', 27, '+79612345511');
-INSERT INTO CUSTOMERS(name, surname, age, phone_number) VALUES('Алексей', 'Сергеев', 33, '+79612342396');
+INSERT INTO customer(name, surname, age, phone_number) VALUES('Иван', 'Иванов', 21, '+799999999999');
+INSERT INTO customer(name, surname, age, phone_number) VALUES('Алексей', 'Иванов', 27, '+79612345511');
+INSERT INTO customer(name, surname, age, phone_number) VALUES('Алексей', 'Сергеев', 33, '+79612342396');
 
-create table ORDERS(
+create table product(
     id int auto_increment,
     date varchar(255),
     customer_id int,
     product_name varchar(255),
     amount int check (amount > 0),
     PRIMARY KEY (id),
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
-INSERT INTO ORDERS(customer_id, date, product_name, amount) VALUES(1, '01.01.2022', 'Сайт', 50000);
-INSERT INTO ORDERS(customer_id, date, product_name, amount) VALUES(2, '01.02.2022', 'Чат-бот', 20000);
-INSERT INTO ORDERS(customer_id, date, product_name, amount) VALUES(3, '01.03.2022', 'Приложение', 200000);
+INSERT INTO product(customer_id, date, product_name, amount) VALUES(1, '01.01.2022', 'Сайт', 50000);
+INSERT INTO product(customer_id, date, product_name, amount) VALUES(2, '01.02.2022', 'Чат-бот', 20000);
+INSERT INTO product(customer_id, date, product_name, amount) VALUES(3, '01.03.2022', 'Приложение', 200000);
